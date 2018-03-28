260ec2fd06a3c854e9e3c5a7605034c4786d29fa
Senthil Rajasekaran <srajasek93@gmail.com>
2018-03-28 15:52:04 -0500

    disorderly_escape
    A solution to the level 5 foobar challenge, disorderly_escape

--- foobar_5.py
+++ foobar_5.py
@@ -1,38 +1,101 @@
-def newsourcesink(sources,sinks, graph):
-    q = [i for i in sources]
-    seen = set([])
-    prev = [-1] * len(graph)
-    while  q:
-        current = q.pop(0)
-        if current in sinks:
-            i = current
-            new_path = [current]
-            while i not in sources:
-                new_path.append(prev[i])
-                i = prev[i]
-            return new_path[::-1]           
-        if current not in seen:
-            seen.add(current)
-            for i in range(len(graph[current])):
-                if graph[current][i] > 0 and i not in seen :
-                    q.append(i)
-                    if prev[i] == -1:
-                        prev[i] = current    
-                    
-    return -1
-# the sources and sinks are guaranteed to be disjoint. if a shortest path is returned, it should be at least length 2
-def edkarp(entrances,exits,path):
-    shortest_path = newsourcesink(entrances,exits,path)
-    m_flow = 0
-    while shortest_path != -1:
-        capacity = path[shortest_path[0]][shortest_path[1]]
-        for i in range(len(shortest_path)-1):
-            if path[shortest_path[i]][shortest_path[i+1]] < capacity:
-                 capacity = path[shortest_path[i]][shortest_path[i+1]]
-        m_flow += capacity
-        for i in range(len(shortest_path)-1):
-            path[shortest_path[i]][shortest_path[i+1]] -= capacity
-            path[shortest_path[i+1]][shortest_path[i]] += capacity
-        
-        shortest_path = newsourcesink(entrances,exits,path)
-    return m_flow
+from math import gcd
+def lcm(a,b):
+    if a == 0 or b == 0:
+        return 0
+    return a*b//(gcd(a,b))
+def parti(n):
+    p = [[[]],[[1]]]
+    while len(p) < n+1:
+        new_e = []
+        for i in range(1,len(p)//2 +1):
+            for j in p[i]:
+                for k in p[len(p) -i]:
+                    b = j+k
+                    b.sort()
+                    if b not in new_e:
+                        new_e.append(j+k)
+        new_e.append([len(p)])
+        p.append(new_e)
+    return p[-1]
+fact_table = [1,
+ 1,
+ 2,
+ 6,
+ 24,
+ 120,
+ 720,
+ 5040,
+ 40320,
+ 362880,
+ 3628800,
+ 39916800,
+ 479001600,
+ 6227020800,
+ 87178291200,
+ 1307674368000,
+ 20922789888000,
+ 355687428096000,
+ 6402373705728000,
+ 121645100408832000,
+ 2432902008176640000,
+ 51090942171709440000,
+ 1124000727777607680000,
+ 25852016738884976640000,
+ 620448401733239439360000,
+ 15511210043330985984000000,
+ 403291461126605635584000000,
+ 10888869450418352160768000000,
+ 304888344611713860501504000000,
+ 8841761993739701954543616000000]
+ 
+def new_fact(n):
+    if n < 30:
+        return fact_table[n]
+    return n * new_fact(n-1)
+    
+def num_cycles(n,k):
+    return new_fact(n) //(k* (new_fact(n-k)))
+def binom(n,k):
+    return new_fact(n) // ((new_fact(k))* new_fact(n-k))
+def to_term(lis):
+    size = sum(lis)
+    powers = [0]*(size)
+    for i in lis:
+        powers[i-1] += 1
+    coeffecient  = 1
+    n = size
+    for i in lis:
+        coeffecient *= num_cycles(n,i)
+        n = n-i
+    for c in powers:
+        coeffecient = coeffecient // new_fact(c)
+    return [coeffecient,powers]
+def get_cycle_index(n):
+    k = parti(n)
+    poly = []
+    for i in k:
+        poly.append(to_term(i))
+    return poly
+def multiply(term1,term2):
+    t= len(term1[1])
+    l= len(term2[1])
+    mult = [term1[0]*term2[0],[0]* (t*l)]
+    for i in range(len(term1[1])):
+        for k in range(len(term2[1])):
+            mult[1][lcm(i+1,k+1) -1] += term1[1][i] * term2[1][k] * gcd(i+1,k+1)
+    return mult
+def cycle_product(n,m):
+    a = get_cycle_index(n)
+    b = get_cycle_index(m)
+    prod = []
+    for i in a:
+        for k in b:
+            prod.append(multiply(i,k))
+    return prod
+def answer(n,m,c):
+    product = cycle_product(n,m)
+    s = 0
+    for k in product:
+        s +=  k[0] * (c**(sum(k[1])))
+    return str( s//(new_fact(m) * new_fact(n)))
+            
