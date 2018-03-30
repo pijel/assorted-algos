9ac61342e13653ade5f93c1cfd3681fbb1e1a3c5
Senthil Rajasekaran <srajasek93@gmail.com>
2018-03-30 18:18:21 -0500

    First Missing Positive
    Returns the first missing positive number in an array, solution to a LeetCode problem

--- first_missing_positive.py
+++ first_missing_positive.py
@@ -7,6 +7,6 @@ def firstMissingPositive(nums):
     if not nums:
         return 1
     k = [i for i in range(1,len(nums)+2)]
-     m = [i for i in k if i not in nums]
+    m = [i for i in k if i not in nums]
     #print(m)
     return min(m)
